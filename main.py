67cef13b40212fb2192d29396da6b9a59c17f522
savent <savent_gate@outlook.com>
2021-03-24 00:40:12 +0800

    feature: split src and dst

--- main.py
+++ main.py
@@ -9,13 +9,17 @@ def isNaN(n):
     return n != n
 
 class dhandle:
-    def __init__(self, file):
+    def __init__(self, file, target_index = 0):
         data = pandas.read_excel(file)
         cols = []
 
         # drop col if has 'NaN'
         height, width = data.shape
+        assert(width > target_index)
+        assert(isNaN(data.columns[target_index]) == False)
         for i in range(0, width):
+            if target_index == i:
+                continue
             valid = True
             for j in data[data.columns[i]]:
                 if isNaN(j):
@@ -27,19 +31,20 @@ class dhandle:
         
         valid_cols = len(cols)
         print("valid cols:{}".format(valid_cols))
-        data = np.zeros((height, valid_cols), dtype=np.float)
+        source = np.zeros((height, valid_cols), dtype=np.float)
         for i in range(valid_cols):
             # data[:, i] = np.array(cols[i].array, dtype=np.float)
-            data[:, i] = cols[i].array
+            source[:, i] = cols[i].array
         
-        print("input matrix: {}".format(data))
+        print("input matrix: {}".format(source))
         self.__cols = cols
-        self.__data = data
+        self.__source = source
+        self.__target = data.columns[target_index]
 
     def doPCA(self):
         pca = PCA(n_components = 1)
         
-        d = pca.fit_transform(self.__data)
+        d = pca.fit_transform(self.__source)
         # inv_d = pca.inverse_transform(self.__data)
 
         print(pca.explained_variance_ratio_)
